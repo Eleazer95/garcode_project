026ef4180ad119fb9143377c1107c36dc329bcd6
user <Pastor Gar>
2024-07-30 21:11:59 +0100

    Second commit

--- garcode_project/settings.py
+++ garcode_project/settings.py
@@ -86,6 +86,17 @@ DATABASES = {
     }
 }
 
+"""DATABASES = {
+    "default": {
+        "ENGINE":"django.db.backends.postgresql",
+        "NAME":"railway",
+        "USER":"postgres",
+        "PASSWORD":"PxmFIiMvzCtZmfDLLmkuBSABctpAadzB",
+        "HOST":"postgres.railway.internal",
+        "PORT":"5432"
+    }
+}
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
